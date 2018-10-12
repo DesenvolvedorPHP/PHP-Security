89478f715015536489251ac56cfceabcb6ed0a37
Fernando Britto <fernandobritto.web@gmail.com>
2018-10-11 21:39:14 -0300

    Fuções em PHP
    Estrutura de uma função no PHP

---
+++ 01_funcoes_PHP.php
@@ -0,0 +1,16 @@
+<?php
+
+function escreverEstados(){
+	echo "São Paulo";
+	echo "<br>";
+	echo "Rio Grande do Sul";
+	echo "<br>";
+	echo "Mato Grosso";
+	echo "<br>";
+	echo "Rio de Janeiro";
+	echo "<br>";
+	echo "Paraná";
+	
+}
+
+escreverEstados();
